596799941eca89d0ae7f373a05d2f1874c653b8e
codemillmatt <msoucoup@codemilltech.com>
2022-02-06 17:15:52 -0800

    Added the web api

---
+++ Weather/Weather.API/Controllers/ReportErrorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Azure.Core.Extensions;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Microsoft.Extensions.Azure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Weather.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportErrorController : ControllerBase
+    {
+        QueueServiceClient _queueServiceClient;
+
+        public ReportErrorController(QueueServiceClient queueServiceClient)
+        {
+            _queueServiceClient = queueServiceClient;
+        }
+
+        // POST api/<ReportErrorController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            var queue = _queueServiceClient.CreateQueue("badforecast");
+
+            queue.Value.SendMessage(value);
+
+            return Ok();
+        }
+    }
+}
