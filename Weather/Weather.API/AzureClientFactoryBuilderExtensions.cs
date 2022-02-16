6d7ce32c045c93a21f12ef0c922acd376c0326c1
codemillmatt <msoucoup@codemilltech.com>
2022-02-16 15:26:17 -0800

    blah

--- Weather/Weather.API/AzureClientFactoryBuilderExtensions.cs
+++ Weather/Weather.API/AzureClientFactoryBuilderExtensions.cs
@@ -19,6 +19,7 @@ internal static class AzureClientFactoryBuilderExtensions
 
     public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
     {
+
         if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri? serviceUri))
         {
             return builder.AddQueueServiceClient(serviceUri);
